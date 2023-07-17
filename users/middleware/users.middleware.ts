afd6bb2867dbb76cb30a031f4e79da2dc267fb19
fjplaurr <fjplaurr@gmail.com>
2023-07-17 22:13:51 +0200

    feat: validate email and password

---
+++ users/middleware/users.middleware.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import UsersService from '../users.service';
+
+class UsersMiddleware {
+  async validateSameEmailDoesntExist(email: string) {
+    const user = await UsersService.getUserByEmailWithPassword(email);
+    if (user) {
+      throw new Error(`User email already exists`);
+    }
+  }
+
+  async extractUserId(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    req.body.id = req.params.userId;
+    next();
+  }
+}
+
+export default new UsersMiddleware();
--- users/users.routes.config.ts
+++ users/users.routes.config.ts
@@ -1,10 +1,29 @@
 import express from 'express';
+import { body } from 'express-validator';
 import UsersController from './users.controller';
 import PostsController from '../posts/posts.controller';
+import UsersMiddleware from './middleware/users.middleware';
+import BodyValidationMiddleware from '../common/middleware/body.validation.middleware';
 
 const router = express.Router();
 
-router.post('/', UsersController.create);
+router.post('/', [
+  body('email')
+    .isEmail()
+    .custom((email) => UsersMiddleware.validateSameEmailDoesntExist(email)),
+  body('password')
+    .isStrongPassword({
+      minLength: 6,
+      minNumbers: 0,
+      minSymbols: 0,
+    })
+    .withMessage(
+      'Please use a password that is at least 6 characters long and includes both lowercase and uppercase letters.'
+    ),
+  BodyValidationMiddleware.verifyBodyFieldsErrors,
+  UsersController.create,
+]);
+
 router.get('/:id', UsersController.readById);
 router.get('/', UsersController.readAll);
 router.patch('/:id', UsersController.updateById);
